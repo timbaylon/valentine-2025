b1856a4b36edd2975d7255a8abb9215ea862c866
Timothy Baylon <103328892+timbaylon@users.noreply.github.com>
2025-02-13 21:04:01 -0600

    added music in config.js

--- config.js
+++ config.js
@@ -79,7 +79,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/doc7s1qrq/video/upload/v1739502134/Pretty_Eyes_4_rxjotl.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
